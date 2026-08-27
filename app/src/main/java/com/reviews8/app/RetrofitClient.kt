730a2a885ea434240b72eea87832a72dc9ac7b65
omar-abdelaziz-mersal <bsbsbsbbsbs@swiftyomail.org>
2026-08-27 03:38:58 +0300

    Add RetrofitClient for API service initialization

---
+++ app/src/main/java/com/reviews8/app/RetrofitClient.kt
@@ -0,0 +1,13 @@
+package com.reviews8.app
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(ApiService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}

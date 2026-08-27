cf5550d3b067d756700dabbde860770484538475
omar-abdelaziz-mersal <bsbsbsbbsbs@swiftyomail.org>
2026-08-27 03:58:40 +0300

    Refactor ApiService data classes and methods

--- app/src/main/java/com/reviews8/app/ApiService.kt
+++ app/src/main/java/com/reviews8/app/ApiService.kt
@@ -4,7 +4,6 @@ import retrofit2.http.POST
 
 data class ApiResponse(val status: String, val msg: String?, val user: UserData?)
 data class UserData(val email: String, val name: String)
-
 data class LoginRequest(val action: String = "login", val email: String, val password: String)
 data class RegisterRequest(val action: String = "register", val email: String, val password: String, val name: String)
 data class SendCodeRequest(val action: String = "send_code", val email: String)
